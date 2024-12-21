6d7e879e55f7a9399b4a701a1efadd38015ea1f3
Bhavik Tank <bhaviktank5@gmail.com>
2024-12-21 16:30:48 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-full bg-black ">
+      <h1 className="thick shadow  text-white text-center font-semibold">
+        Hello World
+      </h1>
+    </div>
+  );
+}
